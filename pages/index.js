34794dbbb6df8c17abdd3f61de9d39157522133c
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-11 16:24:12 +0330

    create main page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Categories from "@/components/modules/Categories";
+import SearchBar from "@/components/modules/SearchBar";
+import CarsPage from "@/components/templates/CarsPage";
+import carsData from "@/data/carsData";
+
 export default function Home() {
+  const cars = carsData.slice(0, 3);
   return (
     <div>
-      <h1>car shop</h1>
+      <SearchBar />
+      <Categories />
+      <CarsPage data={cars} />
     </div>
   );
 }
