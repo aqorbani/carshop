ec4b3f7e0e97bb4f6c27c38f34557b91c637434d
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-11 14:51:51 +0330

    compeleted the category logic & design

--- pages/categories/[type].js
+++ pages/categories/[type].js
@@ -1,7 +1,25 @@
+import Back from "@/components/icons/Back";
+import CarsPage from "@/components/templates/CarsPage";
+import carsData from "@/data/carsData";
 import { useRouter } from "next/router";
+import styles from "@/styles/CategoryPages.module.css";
 
 export default function Type() {
   const router = useRouter();
   const { type } = router.query;
-  return <div>{type}</div>;
+  const cars = carsData.filter((car) => car.category === type);
+  const backHandler = () => {
+    router.back();
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.box}>
+        <div className={styles.back} onClick={backHandler}>
+          <Back />
+          <p>back</p>
+        </div>
+      </div>
+      <CarsPage data={cars} />
+    </div>
+  );
 }
