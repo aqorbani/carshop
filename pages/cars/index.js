ece43026716495707a2d5beba2b995bd149bfe08
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-11 12:57:54 +0330

    create categories module

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -1,9 +1,11 @@
+import Categories from "@/components/modules/Categories";
 import CarsPage from "@/components/templates/CarsPage";
 import carsData from "@/data/carsData";
 
 export default function Cars() {
   return (
     <div>
+      <Categories />
       <CarsPage data={carsData} />
     </div>
   );
